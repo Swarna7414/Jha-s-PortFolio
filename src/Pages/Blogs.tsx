a0d95f064d0852d07e5ca05f574ff579d8fef954
Swarna7414 <swarnasaisankar044@gmail.com>
2025-06-04 23:45:42 -0500

    This is my messages

--- src/Pages/Blogs.tsx
+++ src/Pages/Blogs.tsx
@@ -1,8 +1,126 @@
+import React from "react";
+import { LiaCalendar } from "react-icons/lia";
+import "../Styles/style.css"
+import { AIRadation, ColonSegNet, DoubleUNet, Expandability, MachineLearningbasedclassification, ResUnet, Transformermodel } from "../Services/Blogs";
+
 const Blogs:React.FC=()=>{
-    return(
-        <div>
-            <h1>Contact me</h1>
+  return(
+    <section className="p-5 h-screen bg-black">
+      <h1 className="text-center text-4xl font-semibold cursor-pointer hover:text-blue-300 ">Next-Gen AI Blogs</h1>
+      <div className="mt-5 max-h-[600px] w-full overflow-auto grid grid-cols-1 xl:grid-cols-2 gap-10 p-5">
+        {/**First Grid Box */}
+        <div className="border-[0.5px] h-80 lg:h-60 xl:h-75 w-full rounded-4xl p-2 text-xl group/content hover:shadow-md hover:shadow-white duration-300">
+          <div className="mt-2 ml-2 flex items-center gap-2  shadow-sm hover:shadow-md shadow-blue-400 rounded-2xl px-3 py-1 flex-shrink-0 w-max cursor-progress group/date">
+            <LiaCalendar className="group-hover/date:text-blue-200"/>
+            <h1>February 6, 2023</h1>
+          </div>
+          <h1 className="mt-3 ml-2 font-bold text-center text-lg group-hover/content:text-blue-200 cursor-pointer white-text-shadow">ColonSegNet for Real-time Polyp Segmentation</h1>
+          <div className="mt-4 overflow-hidden break-words text-sm sm:text-base leading-relaxed">
+            <p>Introduction ColonSegNet is a popular lightweight polyp segmentation architecture that has been utilized at an industrial application by NVIDIA for the Clara Holoscan 
+              Sample App for colonoscopy polyps’ segmentation. With an image size of 512*512, ColonSegNet achieves a dice coefficient of 82.06% for the polyp segmentation tasks and 
+              achieves average precision of 80.00% for the polyp detection tasks <span className="text-blue-300 cursor-pointer hover:text-blue-400 hover:underline hover:underline-offset-auto" onClick={ColonSegNet}>Readmore...</span></p>
+          </div>
         </div>
-    );
+
+
+        {/**Second one */}
+
+        <div className="border-[0.5px] h-80 lg:h-60 xl:h-75 w-full rounded-4xl p-2 text-xl group/content hover:shadow-md hover:shadow-white duration-300">
+          <div className="mt-2 ml-2 flex items-center gap-2  shadow-sm hover:shadow-md shadow-blue-400 rounded-2xl px-3 py-1 flex-shrink-0 w-max cursor-progress group/date">
+            <LiaCalendar className="group-hover/date:text-blue-200"/>
+            <h1>February 3, 2023</h1>
+          </div>
+          <h1 className="mt-3 ml-2 font-bold text-center text-lg group-hover/content:text-blue-200 cursor-pointer white-text-shadow">DoubleUNet for Medical image Segmentation</h1>
+          <div className="mt-4 overflow-hidden break-words text-sm sm:text-base leading-relaxed">
+            <p>Semantic image segmentation is the process of labeling each pixel of an image with its 
+              corresponding class. An encoder-decoder based approach, like U-Net and its variants, is a 
+              popular strategy for solving medical image segmentation tasks. To improve the performance of 
+              U-Net on various segmentation tasks, we propose a novel architecture called DoubleU-Net, 
+              Architecture DoubleUNet <span className="text-blue-300 cursor-pointer hover:text-blue-400 hover:underline hover:underline-offset-auto" onClick={DoubleUNet}>Readmore...</span></p>
+          </div>
+        </div>
+
+
+        <div className="border-[0.5px] h-80 lg:h-60 xl:h-75 w-full rounded-4xl p-2 text-xl group/content hover:shadow-md hover:shadow-white duration-300">
+          <div className="mt-2 ml-2 flex items-center gap-2  shadow-sm hover:shadow-md shadow-blue-400 rounded-2xl px-3 py-1 flex-shrink-0 w-max cursor-progress group/date">
+            <LiaCalendar className="group-hover/date:text-blue-200"/>
+            <h1>January 24, 2023</h1>
+          </div>
+          <h1 className="mt-3 ml-2 font-bold text-center text-lg group-hover/content:text-blue-200 cursor-pointer white-text-shadow">Explainablity of deep learning models</h1>
+          <div className="mt-4 overflow-hidden break-words text-sm sm:text-base leading-relaxed">
+            <p>Deep learning models, such as neural networks, have become increasingly popular in recent years 
+              due to their ability to learn and make predictions from large amounts of data. However, one of 
+              the major challenges with these models is that they can be difficult to understand and interpret.
+              This is known as the “black box” problem 
+              <span className="text-blue-300 cursor-pointer hover:text-blue-400 hover:underline hover:underline-offset-auto" onClick={Expandability}> Readmore...</span></p>
+          </div>
+        </div>
+
+
+
+        <div className="border-[0.5px] h-80 lg:h-60 xl:h-75 w-full rounded-4xl p-2 text-xl group/content hover:shadow-md hover:shadow-white duration-300">
+          <div className="mt-2 ml-2 flex items-center gap-2  shadow-sm hover:shadow-md shadow-blue-400 rounded-2xl px-3 py-1 flex-shrink-0 w-max cursor-progress group/date">
+            <LiaCalendar className="group-hover/date:text-blue-200"/>
+            <h1>January 24, 2023</h1>
+          </div>
+          <h1 className="mt-3 ml-2 font-bold text-center text-lg group-hover/content:text-blue-200 cursor-pointer white-text-shadow">Artificial Intelligence in Radiation Therapy for Cancer Treatment</h1>
+          <div className="mt-4 overflow-hidden break-words text-sm sm:text-base leading-relaxed">
+            <p>Radiation therapy (also known as Radiotherapy) is a procedure of cancer treatment that uses high-energy radiation
+              to kill cancer cells. Artificial Intelligence (AI) is revolutionizing the field of cancer treatment, including the 
+              use of radiation therapy. Radiation therapy is a common treatment option for cancer that uses high-energy rays to 
+              target and kill cancer cells. <span className="text-blue-300 cursor-pointer hover:text-blue-400 hover:underline hover:underline-offset-auto" onClick={AIRadation}> Readmore...</span></p>
+          </div>
+        </div>
+
+
+        <div className="border-[0.5px] h-80 lg:h-60 xl:h-75 w-full rounded-4xl p-2 text-xl group/content hover:shadow-md hover:shadow-white duration-300">
+          <div className="mt-2 ml-2 flex items-center gap-2  shadow-sm hover:shadow-md shadow-blue-400 rounded-2xl px-3 py-1 flex-shrink-0 w-max cursor-progress group/date">
+            <LiaCalendar className="group-hover/date:text-blue-200"/>
+            <h1>January 20, 2023</h1>
+          </div>
+          <h1 className="mt-3 ml-2 font-bold text-center text-lg group-hover/content:text-blue-200 cursor-pointer white-text-shadow">Introduction to Transformer Model</h1>
+          <div className="mt-4 overflow-hidden break-words text-sm sm:text-base leading-relaxed">
+            <p>Recently, there has been a significant shift in the field of natural language processing (NLP),
+               towards the use of transformers. Some examples of specific NLP tasks that Transformers have been used for 
+               include Language Translation, Text Generation, Text Summarization, Sentiment Analysis, Dialogue Generation
+               and language modelling.  Transformers, which were first introduced in a 2017{" "}
+               <span className="text-blue-300 cursor-pointer hover:text-blue-400 hover:underline hover:underline-offset-auto" onClick={Transformermodel}>Readmore...</span></p>
+          </div>
+        </div>
+
+
+        <div className="border-[0.5px] h-85 lg:h-60 xl:h-75 w-full rounded-4xl p-2 text-xl group/content hover:shadow-md hover:shadow-white duration-300">
+          <div className="mt-2 ml-2 flex items-center gap-2  shadow-sm hover:shadow-md shadow-blue-400 rounded-2xl px-3 py-1 flex-shrink-0 w-max cursor-progress group/date">
+            <LiaCalendar className="group-hover/date:text-blue-200"/>
+            <h1>January 20, 2023</h1>
+          </div>
+          <h1 className="mt-3 ml-2 font-bold text-center text-lg group-hover/content:text-blue-200 cursor-pointer white-text-shadow">Machine Learning-based Classification, Detection, and Segmentation of Medical Images (PhD Thesis summary)</h1>
+          <div className="mt-4 overflow-hidden break-words text-sm sm:text-base leading-relaxed">
+            <p>Gastrointestinal (GI) cancers are among the most common cancers worldwide. In particular, 
+              colorectal cancer is the most lethal in terms of the number of incidences and mortality 
+              (third most common cause of cancer and the second most common cause of cancer-related deaths).
+               Colonoscopy is the gold standard for screening patients for colorectal cancer. During the {" "}
+               <span className="text-blue-300 cursor-pointer hover:text-blue-400 hover:underline hover:underline-offset-auto" onClick={MachineLearningbasedclassification}>Readmore...</span></p>
+          </div>
+        </div>
+
+
+        <div className="border-[0.5px] h-85 lg:h-60 xl:h-75 w-full rounded-4xl p-2 text-xl group/content hover:shadow-md hover:shadow-white duration-300">
+          <div className="mt-2 ml-2 flex items-center gap-2  shadow-sm hover:shadow-md shadow-blue-400 rounded-2xl px-3 py-1 flex-shrink-0 w-max cursor-progress group/date">
+            <LiaCalendar className="group-hover/date:text-blue-200"/>
+            <h1>January 20, 2023</h1>
+          </div>
+          <h1 className="mt-3 ml-2 font-bold text-center text-lg group-hover/content:text-blue-200 cursor-pointer white-text-shadow">ResUNet++</h1>
+          <div className="mt-4 overflow-hidden break-words text-sm sm:text-base leading-relaxed">
+            <p> ResUNet++ is built upon the Deep Residual U-Net (ResUNet) and UNet. However, ResUNet++ goes further, 
+              embedding: Residual Blocks: Ensuring information propagation across layers. Squeeze and Excitation Blocks: 
+              Recalibrating feature responses for better representation. ASPP (Atrous Spatial Pyramidal Pooling): Enlarging filter’s 
+              field-of-view to capture broader contexts. Attention Blocks: Enhancing the relevance of feature maps in{" "} 
+               <span className="text-blue-300 cursor-pointer hover:text-blue-400 hover:underline hover:underline-offset-auto" onClick={ResUnet}>Readmore...</span></p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 export default Blogs;
